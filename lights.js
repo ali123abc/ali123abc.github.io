e60c5e5c9cf662c1a3becac1283411ae662d9806
Alistair <ajc276@exeter.ac.uk>
2023-04-18 11:55:39 +0100

    Added toggle buttons

--- lights.js
+++ lights.js
@@ -1,48 +1,57 @@
-// Use the Philips Hue API to retrieve the current status of your lights
-fetch('http://192.168.0.5/api/ZknBL-Ryw3Qmi7P73yabgNbc958uXwav2oJhPUtk/lights')
-.then(response => response.json())
-.then(lights => {
-  // Create HTML elements to display the status of each light
-  const statusBox = document.querySelector('.status-box');
-  statusBox.innerHTML = '';
-  Object.keys(lights).forEach(key => {
-    const light = lights[key];
-    if (light.name.includes("Kitchen")) {
-      const div = document.createElement('div');
-      div.innerHTML = `
-        <h2>${light.name}</h2>
-        <p>Brightness: ${light.state.bri}</p>
-        <p>On/Off: ${light.state.on ? 'On' : 'Off'}</p>
-      `;
-      statusBox.appendChild(div);
-    }
-  });
-  if (statusBox.innerHTML === '') {
-    statusBox.innerHTML = 'All kitchen lights are turned off';
-  }
-});
+async function getLights() {
+  try {
+    const response = await fetch('http://192.168.0.5/api/ZknBL-Ryw3Qmi7P73yabgNbc958uXwav2oJhPUtk/lights');
+    const lights = await response.json();
 
-// Use a timer to periodically refresh the information from the API and update the HTML elements
-setInterval(() => {
-fetch('http://192.168.0.5/api/ZknBL-Ryw3Qmi7P73yabgNbc958uXwav2oJhPUtk/lights')
-  .then(response => response.json())
-  .then(lights => {
     const statusBox = document.querySelector('.status-box');
+    statusBox.innerHTML = '';
+
     let allLightsOff = true;
+
     Object.keys(lights).forEach(key => {
       const light = lights[key];
       if (light.name.includes("Kitchen")) {
-        const brightness = statusBox.querySelector(`#${key} p:nth-of-type(1)`);
-        const onOff = statusBox.querySelector(`#${key} p:nth-of-type(2)`);
-        brightness.innerText = `Brightness: ${light.state.bri}`;
-        onOff.innerText = `On/Off: ${light.state.on ? 'On' : 'Off'}`;
+        const div = document.createElement('div');
+        div.innerHTML = `
+          <h2>${light.name}</h2>
+          <p>Brightness: ${light.state.bri}</p>
+          <p>On/Off: ${light.state.on ? 'On' : 'Off'}</p>
+          <button class="toggle-btn" data-light-id="${key}">
+            ${light.state.on ? 'Turn Off' : 'Turn On'}
+          </button>
+        `;
+        statusBox.appendChild(div);
+
+        const toggleButton = div.querySelector('.toggle-btn');
+        toggleButton.addEventListener('click', async () => {
+          const newState = !light.state.on;
+          try {
+            const url = `http://192.168.0.5/api/ZknBL-Ryw3Qmi7P73yabgNbc958uXwav2oJhPUtk/lights/${key}/state`;
+            const method = 'PUT';
+            const body = JSON.stringify({ on: newState });
+            const response = await fetch(url, { method, body });
+            const data = await response.json();
+            toggleButton.textContent = newState ? 'Turn Off' : 'Turn On';
+            const onOff = toggleButton.previousElementSibling;
+            onOff.textContent = `On/Off: ${newState ? 'On' : 'Off'}`;
+          } catch (error) {
+            console.error('Error toggling the light:', error);
+          }
+        });
+
         if (light.state.on) {
           allLightsOff = false;
         }
       }
     });
-    if (allLightsOff) {
+
+    if (statusBox.innerHTML === '') {
       statusBox.innerHTML = 'All kitchen lights are turned off';
     }
-  });
-}, 5000); // Refresh every 5 seconds
+
+  } catch (error) {
+    console.error('Error retrieving lights:', error);
+  }
+}
+
+setInterval(getLights, 5000); // Refresh every 5 seconds
